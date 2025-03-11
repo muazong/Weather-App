9722537a04b59a305297c1ca242d2161b5cda0b3
aZong <giongmua2004@gmail.com>
2025-03-11 20:15:38 +0700

    feat: 🎸 add time period enum

---
+++ src/enums/TimePeriod.ts
@@ -0,0 +1,8 @@
+enum TimePeriod {
+  MORNING = 'morning',
+  NOON = 'noon',
+  AFTERNOON = 'afternoon',
+  EVENING = 'evening',
+}
+
+export default TimePeriod;
---
+++ src/enums/index.ts
@@ -0,0 +1,1 @@
+export { default } from './TimePeriod.ts';
