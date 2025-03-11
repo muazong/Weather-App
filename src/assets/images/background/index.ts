df5864358cb1971672ec096ee9e6829299bbc328
aZong <giongmua2004@gmail.com>
2025-03-11 20:44:06 +0700

    feat: 🎸 add noon background image

--- src/assets/images/background/index.ts
+++ src/assets/images/background/index.ts
@@ -1,9 +1,11 @@
 import morning_background from './morning.jpg';
+import noon_background from './noon.jpg';
 import afternoon_background from './afternoon.jpg';
 import evening_background from './evening.jpg';
 
 const background = {
   morning_background,
+  noon_background,
   afternoon_background,
   evening_background,
 };
