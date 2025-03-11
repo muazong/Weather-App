7229d54a018043d5654d52a4a405e02f4948521d
aZong <giongmua2004@gmail.com>
2025-03-11 20:44:17 +0700

    docs: ✏️ add docs

--- src/enums/TimePeriod.ts
+++ src/enums/TimePeriod.ts
@@ -1,7 +1,17 @@
+/**
+ * Enum representing different periods of the day.
+ */
 enum TimePeriod {
+  /** Morning (00:00 - 05:59) */
   MORNING = 'morning',
+
+  /** Noon (06:00 - 11:59) */
   NOON = 'noon',
+
+  /** Afternoon (12:00 - 17:59) */
   AFTERNOON = 'afternoon',
+
+  /** Evening (18:00 - 23:59) */
   EVENING = 'evening',
 }
 
