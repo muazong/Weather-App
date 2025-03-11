924fc4023d015c850e758250e7a7234955c5cf71
aZong <giongmua2004@gmail.com>
2025-03-11 21:08:37 +0700

    feat: 🎸 add background for main content

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,5 +1,20 @@
 function MainContent() {
-  return <div>MainContent</div>;
+  const background = {
+    background: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: '6px',
+    boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+    backdropFilter: 'blur(5px)',
+    border: '1px solid rgba(255, 255, 255, 0.2)',
+  };
+
+  return (
+    <div
+      style={background}
+      className="flex justify-center items-center h-[95%] max-w-[1200px] w-full"
+    >
+      MainContent
+    </div>
+  );
 }
 
 export default MainContent;
