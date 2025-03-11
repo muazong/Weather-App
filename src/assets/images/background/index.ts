704aa1d1e4e8a94ce231acc9176f403bb6aa051d
aZong <giongmua2004@gmail.com>
2025-03-11 20:01:11 +0700

    feat: 🎸 add background folder

---
+++ src/assets/images/background/index.ts
@@ -0,0 +1,11 @@
+import morning_background from './morning.jpg';
+import afternoon_background from './afternoon.jpg';
+import evening_background from './evening.jpg';
+
+const background = {
+  morning_background,
+  afternoon_background,
+  evening_background,
+};
+
+export default background;
